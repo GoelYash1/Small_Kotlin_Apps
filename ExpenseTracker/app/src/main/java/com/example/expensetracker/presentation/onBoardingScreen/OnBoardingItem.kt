3eb71dc49add358cbbb0a1c767a7e909683ca403
Yash Goel <89704419+GoelYash1@users.noreply.github.com>
2023-06-07 00:11:01 +0530

    Initial Commit with Onboarding Screen

---
+++ ExpenseTracker/app/src/main/java/com/example/expensetracker/presentation/onBoardingScreen/OnBoardingItem.kt
@@ -0,0 +1,34 @@
+package com.example.expensetracker.presentation.onBoardingScreen
+
+import com.example.expensetracker.R
+
+data class OnBoardingItem(
+    val onBoardingImage: Int,
+    val title: String,
+    val description: String
+)
+
+fun GetOnBoardingItemList(): List<OnBoardingItem> {
+    return listOf(
+        OnBoardingItem(
+            onBoardingImage = R.drawable.onbg1,
+            title = "Start Tracking",
+            description = "Stay on top of your finances with ease"
+        ),
+        OnBoardingItem(
+            onBoardingImage = R.drawable.onbg2,
+            title = "Set Budgets",
+            description = "Manage your spending by setting monthly budgets"
+        ),
+        OnBoardingItem(
+            onBoardingImage = R.drawable.onbg3,
+            title = "Categorize Expenses",
+            description = "Organize your expenses into custom categories for better tracking"
+        ),
+        OnBoardingItem(
+            onBoardingImage = R.drawable.onbg4,
+            title = "Automatic Expense Tracking",
+            description = "Effortlessly track your expenses by analyzing SMS messages"
+        )
+    )
+}
---
+++ ExpenseTracker/app/src/main/java/com/example/expensetracker/presentation/onBoardingScreen/OnBoardingScreen.kt
@@ -0,0 +1,134 @@
+package com.example.expensetracker.presentation.onBoardingScreen
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.ArrowForward
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun OnBoardingScreen() {
+    val onBoardingItems = GetOnBoardingItemList()
+    var currentItemIndex by remember { mutableStateOf(0) }
+    var onBoardingCompleted by remember { mutableStateOf(false) }
+
+    Column(
+        Modifier
+            .padding(20.dp)
+            .fillMaxSize()
+    ) {
+        if (onBoardingCompleted) {
+            // Navigate to the next screen
+            // Replace with your navigation code here
+        } else {
+            UpperOnBoardingPanel(onBoardingItems, currentItemIndex)
+            LowerOnBoardingPanel(
+                onBoardingItems,
+                currentItemIndex,
+                onBoardingCompleted = { onBoardingCompleted = it },
+                onNextClick = { currentItemIndex++ },
+                onPreviousClick = { currentItemIndex-- }
+            )
+        }
+    }
+}
+
+@Composable
+fun UpperOnBoardingPanel(onBoardingItems: List<OnBoardingItem>, currentItemIndex: Int) {
+    Box(
+        modifier = Modifier
+            .fillMaxHeight(0.6f)
+            .fillMaxWidth()
+    ) {
+        Image(
+            painter = painterResource(id = onBoardingItems[currentItemIndex].onBoardingImage),
+            contentDescription = "",
+            modifier = Modifier.fillMaxSize()
+        )
+    }
+}
+
+@Composable
+fun LowerOnBoardingPanel(
+    onBoardingItems: List<OnBoardingItem>,
+    currentItemIndex: Int,
+    onBoardingCompleted: (Boolean) -> Unit,
+    onNextClick: () -> Unit,
+    onPreviousClick: () -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+    ) {
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = onBoardingItems[currentItemIndex].title,
+                fontSize = 32.sp,
+                fontWeight = FontWeight.ExtraBold,
+                textAlign = TextAlign.Center,
+                lineHeight = 28.sp
+            )
+            Spacer(modifier = Modifier.padding(15.dp))
+            Text(
+                text = onBoardingItems[currentItemIndex].description,
+                fontSize = 20.sp,
+                textAlign = TextAlign.Center
+            )
+            Spacer(modifier = Modifier.padding(15.dp))
+            Row(
+                modifier = Modifier.fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.SpaceEvenly
+            ) {
+                if(currentItemIndex>0){
+                    Icon(
+                        imageVector = Icons.Default.ArrowBack,
+                        contentDescription = "",
+                        modifier = Modifier
+                            .clickable { onPreviousClick() }
+                    )
+                }
+                Icon(
+                    imageVector = Icons.Default.ArrowForward,
+                    contentDescription = "",
+                    modifier = Modifier
+                        .clickable {
+                            if (currentItemIndex == onBoardingItems.lastIndex) {
+                                onBoardingCompleted(true)
+                            } else {
+                                onNextClick()
+                            }
+                        }
+                )
+            }
+        }
+    }
+}
+
